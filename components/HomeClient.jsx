b0e2c6946bd081e965d68de73f36efb4c7102fc1
greatnathan0721-pixel <greatnathan0721@gmail.com>
2025-09-12 16:37:52 +0800

    Delete components/HomeClient.jsx

--- components/HomeClient.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function HomeClient() {
-  return (
-    <main style={{ maxWidth: 720, margin: '40px auto', fontFamily: 'sans-serif', padding: '0 16px' }}>
-      <h1>Hello World</h1>
-    </main>
-  );
-}
