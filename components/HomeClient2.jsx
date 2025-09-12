ef899d5dc449537e520dc140158ea8a8474552ec
greatnathan0721-pixel <greatnathan0721@gmail.com>
2025-09-12 16:36:19 +0800

    Create HomeClient2.jsx

---
+++ components/HomeClient2.jsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function HomeClient() {
+  return (
+    <main style={{ maxWidth: 720, margin: '40px auto', fontFamily: 'sans-serif', padding: '0 16px' }}>
+      <h1>Hello World (HomeClient2)</h1>
+    </main>
+  );
+}
